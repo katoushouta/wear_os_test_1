2213bd7b0946c4d0c56ece1edc92345c09f60ea4
katoushouta <shouta.katou@link-u.co.jp>
2023-09-11 15:39:13 +0900

    firsst commite (アプリコンポーズに対応済み)

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "wear_test_1"
+include(":mobile")
+include(":wear")
