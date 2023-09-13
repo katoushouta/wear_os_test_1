ba152252422ba361a719ac4115c42b59aa0bacb3
katoushouta <shouta.katou@link-u.co.jp>
2023-09-13 22:00:05 +0900

    update: ロータリーでスクロール

--- wear/src/main/java/com/example/wear_test_1/presentation/MainActivity.kt
+++ wear/src/main/java/com/example/wear_test_1/presentation/MainActivity.kt
@@ -13,22 +13,33 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
@@ -37,13 +48,16 @@ import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Scaffold
+import androidx.wear.compose.material.ScalingLazyColumn
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
+import androidx.wear.compose.material.rememberScalingLazyListState
 import com.example.wear_test_1.R
 import com.example.wear_test_1.presentation.theme.Wear_test_1Theme
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.PutDataMapRequest
 import com.google.android.gms.wearable.Wearable
+import kotlinx.coroutines.launch
 
 private const val TO_PHONE_PATH = "/vital"
 private const val COUNT_KEY = "com.example.wear_test_1.count"
@@ -84,40 +98,68 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun WearApp(greetingName: String, increaseCounter: (dispatcher: (prevCount: Int) -> Int) -> Unit) {
-    val scrollState = rememberScrollState()
+    val listState = rememberScalingLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val focusRequster = remember {FocusRequester()}
 
     Wear_test_1Theme {
         /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
          * version of LazyColumn for wear devices with some added features. For more information,
          * see d.android.com/wear/compose.
          */
-        Column(
+        ScalingLazyColumn(
             modifier = Modifier
-                .verticalScroll(scrollState)
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
+                .fillMaxWidth()
+                .background(MaterialTheme.colors.background)
+                .onRotaryScrollEvent {
+                    Log.d("scroll", "val : ${it.verticalScrollPixels}")
+                    coroutineScope.launch {
+                        listState.scrollBy(it.verticalScrollPixels)
+                    }
+                    true
+                }
+                .focusRequester(focusRequster)
+                .focusable(),
+            state = listState,
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 0.dp)
         ) {
-            Greeting(greetingName = greetingName)
-            Spacer(modifier = Modifier.height(8.dp))
-            Button(onClick = {
-                increaseCounter {
-                    it + 1
-                }
-            }) {
-                Text("+", Modifier.padding(all = 16.dp))
+            item {
+                Greeting(greetingName = greetingName)
             }
-            Spacer(modifier = Modifier.width(8.dp))
-            Button(onClick = {
-                increaseCounter {
-                    0
-                }
-            }) {
-                Text("C", Modifier.padding(all = 16.dp))
+            item {
+                Spacer(modifier = Modifier.height(8.dp))
             }
+            item {
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                    increaseCounter {
+                        it + 1
+                    }
+                }) {
+                    Text("+", Modifier.padding(all = 16.dp))
+                }
+            }
+            item { Spacer(modifier = Modifier.height(8.dp)) }
+            item {
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                    increaseCounter {
+                        0
+                    }
+                }) {
+                    Text("C", Modifier.padding(all = 16.dp))
+                }
+            }
+        }
+        LaunchedEffect(key1 = Unit) {
+            focusRequster.requestFocus()
         }
     }
 }
